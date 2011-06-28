f0b063c1828f71daa86fa1ecc3b48cdd7a432590
Ernest Park <ernest@ooyala.com>
2011-06-28 10:37:21 -0700

    pre 6.28

---
+++ assets/krapy.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$("#newsletter-link").click(function(){
+		$("#newsletter-box").show();
+
+	});
+	$("#newsletter-close").click(function(){
+		$("#newsletter-box").hide();
+	});
+
+
+
+});
